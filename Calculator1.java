dd21d7059d051e447702f72f8bc4dc0bf5c7c2d2
DARRURUSUPRAJA <darrurusupraja@gmail.com>
2026-01-09 10:47:57 +0530

    Add files via upload

---
+++ Calculator1.java
@@ -0,0 +1,22 @@
+public class Calculator1 {
+    public static void main(String[] args) {
+        int a = 10;   // first number
+        int b = 5;    // second number
+
+        // Addition
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+
+        // Subtraction
+        int difference = a - b;
+        System.out.println("Difference: " + difference);
+
+        // Multiplication
+        int product = a * b;
+        System.out.println("Product: " + product);
+
+        // Division
+        int quotient = a / b;
+        System.out.println("Quotient: " + quotient);
+    }
+}
